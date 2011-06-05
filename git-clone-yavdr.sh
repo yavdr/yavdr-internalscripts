93e675436fe4ad137adc52ffa4e83bc3c8e997f0
Marco Scholl <develop@marco-scholl.de>
2011-06-05 09:47:59 -0700

    comments

--- git-clone-yavdr.sh
+++ git-clone-yavdr.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# here you can configure the repos
 REPOS="yavdr yavdr-addon-asunder yavdr-addon-chromium yavdr-addon-claws-mail yavdr-addon-dvdrip yavdr-addon-helix yavdr-addon-mame yavdr-addon-mediathek yavdr-addon-performous yavdr-addon-pidgin yavdr-addon-pip yavdr-addon-semsix yavdr-addon-simfy yavdr-addon-skype yavdr-addon-supertuxkart yavdr-addon-thunderbird yavdr-addon-tuxracer yavdr-addon-tvbrowser yavdr-addon-vlc yavdr-addon-wormux yavdr-addon-youtube yavdr-base yavdr-dashboard yavdr-dev yavdr-essential yavdr-hardware-mdm166a yavdr-hardware-sundtek yavdr-i18n yavdr-internalscripts yavdr-test yavdr-utils yavdr-webfrontend yavdr-remote yavdr-dkms-scripts"
 
+# clone or pull repos
 for REPO in $REPOS
 do
   if [ ! -d $REPO ]; then

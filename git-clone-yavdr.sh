e3fb68761a9d7ef227cf56a178148c6bb5cef65f
Marco Scholl <develop@marco-scholl.de>
2011-06-05 20:12:51 +0200

    add yavdr-plymouth-theme

--- git-clone-yavdr.sh
+++ git-clone-yavdr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPOS="yavdr yavdr-addon-asunder yavdr-addon-chromium yavdr-addon-claws-mail yavdr-addon-dvdrip yavdr-addon-helix yavdr-addon-mame yavdr-addon-mediathek yavdr-addon-performous yavdr-addon-pidgin yavdr-addon-pip yavdr-addon-semsix yavdr-addon-simfy yavdr-addon-skype yavdr-addon-supertuxkart yavdr-addon-thunderbird yavdr-addon-tuxracer yavdr-addon-tvbrowser yavdr-addon-vlc yavdr-addon-wormux yavdr-addon-youtube yavdr-base yavdr-dashboard yavdr-dev yavdr-essential yavdr-hardware-mdm166a yavdr-hardware-sundtek yavdr-i18n yavdr-internalscripts yavdr-test yavdr-utils yavdr-webfrontend yavdr-remote yavdr-dkms-scripts"
+REPOS="yavdr yavdr-addon-asunder yavdr-addon-chromium yavdr-addon-claws-mail yavdr-addon-dvdrip yavdr-addon-helix yavdr-addon-mame yavdr-addon-mediathek yavdr-addon-performous yavdr-addon-pidgin yavdr-addon-pip yavdr-addon-semsix yavdr-addon-simfy yavdr-addon-skype yavdr-addon-supertuxkart yavdr-addon-thunderbird yavdr-addon-tuxracer yavdr-addon-tvbrowser yavdr-addon-vlc yavdr-addon-wormux yavdr-addon-youtube yavdr-base yavdr-dashboard yavdr-dev yavdr-essential yavdr-hardware-mdm166a yavdr-hardware-sundtek yavdr-i18n yavdr-internalscripts yavdr-test yavdr-utils yavdr-webfrontend yavdr-remote yavdr-dkms-scripts yavdr-plymouth-theme"
 
 for REPO in $REPOS
 do

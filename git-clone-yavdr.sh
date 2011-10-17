b78070e7416944121d1dd6fa172ed600571783f8
Lars Hanisch <dvb@flensrocker.de>
2011-10-18 00:27:17 +0200

    get all repository names via REST from github, needs curl, grep, sort and awk

--- git-clone-yavdr.sh
+++ git-clone-yavdr.sh
@@ -1,61 +1,7 @@
 #!/bin/bash
 
-# here you can configure the repos
-REPOS="\
- channelpedia\
- vdr\
- vdr-addon-avahi-mounter\
- vdr-addon-epgdata2vdr\
- vdr-plugin-dbus\
- vdr-plugin-dbus2vdr\
- vdr-plugin-dynamite\
- vdr-plugin-jsonapi\
- vdr-plugin-pvrinput\
- vdr-plugin-restfulapi\
- vdr-plugin-shutdown\
- vdr-plugin-sundtek\
- vtuner\
- yavdr\
- yavdr-addon-asunder\
- yavdr-addon-chromium\
- yavdr-addon-claws-mail\
- yavdr-addon-dvdrip\
- yavdr-addon-helix\
- yavdr-addon-mame\
- yavdr-addon-mediathek\
- yavdr-addon-performous\
- yavdr-addon-pidgin\
- yavdr-addon-pip\
- yavdr-addon-semsix\
- yavdr-addon-simfy\
- yavdr-addon-skype\
- yavdr-addon-supertuxkart\
- yavdr-addon-thunderbird\
- yavdr-addon-tuxracer\
- yavdr-addon-tvbrowser\
- yavdr-addon-vlc\
- yavdr-addon-wormux\
- yavdr-addon-youtube\
- yavdr-base\
- yavdr-dashboard\
- yavdr-dev\
- yavdr-dkms-scripts\
- yavdr-doc\
- yavdr-essential\
- yavdr-hardware-displaylink\
- yavdr-hardware-mdm166a\
- yavdr-hardware-sundtek\
- yavdr-i18n\
- yavdr-internalscripts\
- yavdr-internal-server\
- yavdr-plymouth-theme\
- yavdr-remote\
- yavdr-test\
- yavdr-utils\
- yavdr-webfrontend"
-
 # clone or pull repos
-for REPO in $REPOS
+for REPO in `curl -s https://api.github.com/users/yavdr/repos?per_page=200 | grep name | sort | awk -F \" '{ print $4 }'`
 do
   if [ ! -d $REPO ]; then
     if [ "$READONLY" = "1" ]; then

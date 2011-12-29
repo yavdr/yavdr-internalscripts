0b1c33a2f95fa1e29cb35b621f021caf1109b7a7
Holger Schvestka <hotzenplotz5@gmx.de>
2011-12-29 10:50:43 +0100

    rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -4,13 +4,154 @@
 
 #DIST=$DISTRIB_CODENAME
 #DIST="lucid"
-DIST="natty"
-#DIST="oneiric"
+#DIST="natty"
+DIST="oneiric"
 #DIST="precise"
 
 mkdir /tmp/build
 
-  PACKAGES="vdr-plugin-skinpearlhd vdr-plugin-graphtft vdr-plugin-markad vdr-plugin-iptv vdr-plugin-wirbelscan vdr-plugin-streamdev vdr-plugin-extrecmenu vdr-plugin-femon vdr-plugin-text2skin vdr-plugin-live vdr-plugin-dbus2vdr vdr-plugin-sundtek vdr-plugin-xine vdr-plugin-xineliboutput vdr-plugin-channellists vdr-plugin-menuorg vdr-plugin-dummydevice vdr-plugin-dynamite vdr-plugin-control vdr-plugin-dbus vdr-plugin-advchctrl vdr-plugin-alcd vdr-plugin-androvdr vdr-plugin-arghdirector vdr-plugin-atscepg vdr-plugin-autosort vdr-plugin-autostart vdr-plugin-avards vdr-plugin-bewegung vdr-plugin-bgprocess vdr-plugin-block vdr-plugin-burn vdr-plugin-calc vdr-plugin-cdda vdr-plugin-cdplayer vdr-plugin-chanorg vdr-plugin-cinebars vdr-plugin-devstatus vdr-plugin-director vdr-plugin-dvd vdr-plugin-dvdswitch vdr-plugin-epgsearch vdr-plugin-extb vdr-plugin-fepg vdr-plugin-filebrowser vdr-plugin-freecell vdr-plugin-fritzbox vdr-plugin-games vdr-plugin-image vdr-plugin-imdbsearch vdr-plugin-imonlcd vdr-plugin-infosatepg vdr-plugin-karaoke vdr-plugin-lastfm vdr-plugin-lcdproc vdr-plugin-lcr vdr-plugin-lircrc vdr-plugin-loadepg vdr-plugin-mailbox vdr-plugin-mcli vdr-plugin-mlist vdr-plugin-mousemate vdr-plugin-mp3 vdr-plugin-muggle vdr-plugin-music vdr-plugin-newsticker vdr-plugin-noepgmenu vdr-plugin-nordlichtsepg vdr-plugin-osdpip vdr-plugin-osdserver vdr-plugin-osdteletext vdr-plugin-osdtest256 vdr-plugin-pilot vdr-plugin-pim vdr-plugin-pin vdr-plugin-playlist vdr-plugin-podcatcher vdr-plugin-prefermenu vdr-plugin-proxy vdr-plugin-pvr350 vdr-plugin-radio vdr-plugin-radiolist vdr-plugin-recstatus vdr-plugin-remote vdr-plugin-remoteosd vdr-plugin-remotetimers vdr-plugin-rotor vdr-plugin-rssreader vdr-plugin-scheduler vdr-plugin-screenshot vdr-plugin-skinelchi vdr-plugin-skinenigmang vdr-plugin-skinsoppalusikka vdr-plugin-sleeptimer vdr-plugin-span vdr-plugin-spider vdr-plugin-suspendoutput vdr-plugin-systeminfo  vdr-plugin-targavfd vdr-plugin-trayopenng vdr-plugin-webvideo vdr-plugin-xmltv2vdr vdr-plugin-zappilot vdr-plugin-zaphistory vdr-plugin-yaepghd vdr-plugin-tvtv vdr-plugin-sudoku vdr-plugin-tvonscreen vdr-plugin-upnp vdr-plugin-sndctl vdr-plugin-svdrposd vdr-plugin-powermate vdr-plugin-svdrpservice vdr-plugin-vdrrip vdr-plugin-picselshow vdr-plugin-vompserver vdr-plugin-clock vdr-plugin-externalplayer vdr-plugin-serial vdr-plugin-solitaire vdr-plugin-vcd vdr-plugin-vdrc vdr-plugin-vdrcd vdr-plugin-ttxtsubs vdr-plugin-undelete vdr-plugin-timeline vdr-plugin-timersync vdr-plugin-surfer vdr-plugin-dvbhddevice vdr-plugin-shutdown vdr-plugin-graphlcd vdr-plugin-epgsync vdr-plugin-pvrinput vdr-plugin-favorites vdr-plugin-rotorng vdr-plugin-xvdr vdr-plugin-restfulapi vdr-plugin-atmo vdr-plugin-duplicates vdr-plugin-softhddevice vdr-plugin-eepg"
+PACKAGES="vdr-plugin-advchctrl \
+vdr-plugin-alcd \
+vdr-plugin-androvdr \
+vdr-plugin-arghdirector \
+vdr-plugin-atmo \
+vdr-plugin-atscepg \
+vdr-plugin-autosort \
+vdr-plugin-autostart \
+vdr-plugin-avards \
+vdr-plugin-bewegung \
+vdr-plugin-bgprocess \
+vdr-plugin-block \
+vdr-plugin-burn \
+vdr-plugin-calc \
+vdr-plugin-cdda \
+vdr-plugin-cdplayer \
+vdr-plugin-channellists \
+vdr-plugin-chanorg \
+vdr-plugin-cinebars \
+vdr-plugin-clock \
+vdr-plugin-control \
+vdr-plugin-dbus \
+vdr-plugin-dbus2vdr \
+vdr-plugin-ddci \
+vdr-plugin-devstatus \
+vdr-plugin-director \
+vdr-plugin-dummydevice \
+vdr-plugin-duplicates \
+vdr-plugin-dvbhddevice \
+vdr-plugin-dvd \
+vdr-plugin-dvdswitch \
+vdr-plugin-dynamite \
+vdr-plugin-eepg \
+vdr-plugin-epgsearch \
+vdr-plugin-epgsync \
+vdr-plugin-extb \
+vdr-plugin-externalplayer \
+vdr-plugin-extrecmenu \
+vdr-plugin-favorites \
+vdr-plugin-femon \
+vdr-plugin-fepg \
+vdr-plugin-filebrowser \
+vdr-plugin-freecell \
+vdr-plugin-fritzbox \
+vdr-plugin-games \
+vdr-plugin-graphlcd \
+vdr-plugin-graphtft \
+vdr-plugin-image \
+vdr-plugin-imdbsearch \
+vdr-plugin-imonlcd \
+vdr-plugin-infosatepg \
+vdr-plugin-iptv \
+vdr-plugin-karaoke \
+vdr-plugin-lastfm \
+vdr-plugin-lcdproc \
+vdr-plugin-lcr \
+vdr-plugin-lircrc \
+vdr-plugin-live \
+vdr-plugin-loadepg \
+vdr-plugin-mailbox \
+vdr-plugin-markad \
+vdr-plugin-mcli \
+vdr-plugin-menuorg \
+vdr-plugin-mlist \
+vdr-plugin-mousemate \
+vdr-plugin-mp3 \
+vdr-plugin-muggle \
+vdr-plugin-music \
+vdr-plugin-newsticker \
+vdr-plugin-noepgmenu \
+vdr-plugin-nordlichtsepg \
+vdr-plugin-osdpip \
+vdr-plugin-osdserver \
+vdr-plugin-osdteletext \
+vdr-plugin-osdtest256 \
+vdr-plugin-picselshow \
+vdr-plugin-pilot \
+vdr-plugin-pim \
+vdr-plugin-pin \
+vdr-plugin-playlist \
+vdr-plugin-podcatcher \
+vdr-plugin-powermate \
+vdr-plugin-prefermenu \
+vdr-plugin-proxy \
+vdr-plugin-pvr350 \
+vdr-plugin-pvrinput \
+vdr-plugin-radio \
+vdr-plugin-radiolist \
+vdr-plugin-recstatus \
+vdr-plugin-remote \
+vdr-plugin-remoteosd \
+vdr-plugin-remotetimers \
+vdr-plugin-restfulapi \
+vdr-plugin-rotor \
+vdr-plugin-rotorng \
+vdr-plugin-rssreader \
+vdr-plugin-scheduler \
+vdr-plugin-screenshot \
+vdr-plugin-serial \
+vdr-plugin-shutdown \
+vdr-plugin-skinelchi \
+vdr-plugin-skinenigmang \
+vdr-plugin-skinpearlhd \
+vdr-plugin-skinsoppalusikka \
+vdr-plugin-sleeptimer \
+vdr-plugin-sndctl \
+vdr-plugin-softhddevice \
+vdr-plugin-solitaire \
+vdr-plugin-span \
+vdr-plugin-spider \
+vdr-plugin-streamdev \
+vdr-plugin-sudoku \
+vdr-plugin-sundtek \
+vdr-plugin-surfer \
+vdr-plugin-suspendoutput \
+vdr-plugin-svdrposd \
+vdr-plugin-svdrpservice \
+vdr-plugin-systeminfo \ 
+vdr-plugin-targavfd \
+vdr-plugin-text2skin \
+vdr-plugin-timeline \
+vdr-plugin-timersync \
+vdr-plugin-trayopenng \
+vdr-plugin-ttxtsubs \
+vdr-plugin-tvonscreen \
+vdr-plugin-tvtv \
+vdr-plugin-undelete \
+vdr-plugin-upnp \
+vdr-plugin-vcd \
+vdr-plugin-vdrc \
+vdr-plugin-vdrcd \
+vdr-plugin-vdrrip \
+vdr-plugin-vompserver \
+vdr-plugin-webvideo \
+vdr-plugin-wirbelscan \
+vdr-plugin-xine \
+vdr-plugin-xineliboutput \
+vdr-plugin-xmltv2vdr \
+vdr-plugin-xvdr \
+vdr-plugin-yaepghd \
+vdr-plugin-zaphistory \
+vdr-plugin-zappilot"
 
 
 for PACKAGE in $PACKAGES

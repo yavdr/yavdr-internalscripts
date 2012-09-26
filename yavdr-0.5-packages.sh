46d2dd53dde9c52a335de85dfda9a14ba61011c6
Lars Hanisch <dvb@flensrocker.de>
2012-09-27 01:00:17 +0200

    add helper script for updating git branches

---
+++ yavdr-0.5-packages.sh
@@ -0,0 +1,46 @@
+# all packages managed in the git repositories at github
+# for yaVDR 0.5
+
+PACKAGES="yavdr-addon-asunder \
+yavdr-addon-chromium \
+yavdr-addon-claws-mail \
+yavdr-addon-dvdrip \
+yavdr-addon-helix \
+yavdr-addon-mame \
+yavdr-addon-mediathek \
+yavdr-addon-performous \
+yavdr-addon-pidgin \
+yavdr-addon-pip \
+yavdr-addon-semsix \
+yavdr-addon-simfy \
+yavdr-addon-skype \
+yavdr-addon-supertuxkart \
+yavdr-addon-thunderbird \
+yavdr-addon-tuxracer \
+yavdr-addon-vlc \
+yavdr-addon-wormux \
+yavdr-addon-youtube \
+yavdr-backup \
+yavdr-base \
+yavdr-desktop \
+yavdr-dev \
+yavdr-dvb \
+yavdr-essential \
+yavdr-hardware-displaylink \
+yavdr-hardware-mdm166a \
+yavdr-hardware-sundtek \
+yavdr-i18n \
+yavdr-plymouth-theme \
+yavdr-remote \
+yavdr-utils \
+yavdr-webfrontend \
+vdr-plugin-dbus \
+vdr-plugin-dbus2vdr \
+vdr-plugin-ddci \
+vdr-plugin-dynamite \
+vdr-plugin-noepg \
+vdr-plugin-pvrinput \
+vdr-plugin-restfulapi \
+vdr-plugin-shutdown \
+vdr-plugin-sundtek"
+
---
+++ yavdr-branch.sh
@@ -0,0 +1,216 @@
+#!/bin/bash
+
+VERSION=0.5
+URL=git@github.com:yavdr/
+BASE=master
+REMOTE=origin
+TARGET=testing-$VERSION
+OPTTARGET=
+WORKINGDIR=/tmp/$0
+
+while getopts "b:r:t:u:v:w:" opt; do
+  case $opt in
+    b)
+      BASE=$OPTARG
+      ;;
+    r)
+      REMOTE=$OPTARG
+      ;;
+    t)
+      OPTTARGET=$OPTARG
+      ;;
+    u)
+      URL=$OPTARG
+      ;;
+    v)
+      VERSION=$OPTARG
+      ;;
+    w)
+      WORKINGDIR=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ -n "$OPTTARGET" ]
+then
+  TARGET=$OPTTARGET-$VERSION
+fi
+
+echo "version = $VERSION"
+echo "base = $BASE"
+echo "remote = $REMOTE"
+echo "target = $TARGET"
+echo "working dir = $WORKINGDIR"
+
+if [ ! -f yavdr-$VERSION-packages.sh ]
+then
+  echo "package-list for version $VERSION not found." >&2
+  exit 1
+fi
+
+. ./yavdr-$VERSION-packages.sh
+
+echo $REMOTE/$BASE to $TARGET
+
+# if not directory exists
+#   if not clone repository ok
+#     skip package
+
+# if not update repository ok
+#   skip package
+
+# if not base branch exists remote
+#   skip package
+
+# if not base branch exists local
+#   create new local base branch from remote base
+# else if base branch exists local
+#   checkout local base branch
+# if not update local base branch ok
+#   skip package
+
+# if not target branch exists remote
+#   if not target branch exists local
+#     create new target branch from remote base
+#     push package
+#   else if target branch exists local
+#     skip package
+
+# if target branch exists remote
+#   if not target branch exists local
+#     create new target branch from remote target
+#   else if target branch exists local
+#     update local target branch
+
+# if merge with local base ok
+#   push package
+
+mkdir -p $WORKINGDIR
+
+for PACKAGE in $PACKAGES
+do
+  cd $WORKINGDIR
+  echo processing $PACKAGE
+
+  if [ ! -d $PACKAGE ]
+  then
+    git clone $URL$PACKAGE.git $PACKAGE
+    if [ $? -ne 0 ]
+    then
+      echo "can't clone $PACKAGE, skipping" >&2
+      continue
+    fi
+  fi
+
+  # update repository
+  cd $PACKAGE
+  git fetch --all
+  if [ $? -ne 0 ]
+  then
+    echo "can't update $PACKAGE, skipping" >&2
+    continue
+  fi
+
+  # if not base branch exists remote
+  git ls-remote --exit-code --heads $REMOTE "refs/heads/$BASE"
+  if [ $? -ne 0 ]
+  then
+    echo "can't find base branch $BASE, skipping package" >&2
+    continue
+  fi
+
+  # if not base branch exists local
+  git show-ref --verify --quiet "refs/heads/$BASE"
+  if [ $? -ne 0 ]
+  then
+    git checkout -b $BASE $REMOTE/$BASE
+    if [ $? -ne 0 ]
+    then
+      echo "can't create base branch $BASE, skipping" >&2
+      continue
+    fi
+  else
+    git checkout $BASE
+    if [ $? -ne 0 ]
+    then
+      echo "can't checkout base branch $BASE, skipping" >&2
+      continue
+    fi
+    git pull
+    if [ $? -ne 0 ]
+    then
+      echo "can't update base branch $BASE, skipping" >&2
+      continue
+    fi
+  fi
+  # now local base branch is up-to-date
+
+  # if not target branch exists remote
+  git ls-remote --exit-code --heads $REMOTE "refs/heads/$TARGET"
+  if [ $? -ne 0 ]
+  then
+    # if not target branch exists local
+    git show-ref --verify --quiet "refs/heads/$TARGET"
+    if [ $? -ne 0 ]
+    then
+      echo "target branch $TARGET does not exists, creating..."
+      git checkout -b $TARGET $REMOTE/$BASE
+      if [ $? -ne 0 ]
+      then
+        echo "can't create target branch $TARGET, skipping..." >&2
+      else
+        echo "pushing to $REMOTE..."
+        git push -u $REMOTE $TARGET
+      fi
+    else
+      echo "unexpected local branch $TARGET, skipping" >&2
+    fi
+    continue
+  fi
+
+  echo "update target branch $TARGET..."
+  # if not target branch exists local
+  git show-ref --verify --quiet "refs/heads/$TARGET"
+  if [ $? -ne 0 ]
+  then
+    git checkout -b $TARGET $REMOTE/$TARGET
+    if [ $? -ne 0 ]
+    then
+      echo "can't create local target branch $TARGET, skipping" >&2
+      continue
+    fi
+  else  
+    git checkout $TARGET
+    if [ $? -ne 0 ]
+    then
+      echo "can't checkout local target branch $TARGET, skipping" >&2
+      continue
+    fi
+    git pull
+    if [ $? -ne 0 ]
+    then
+      echo "can't update local target branch $TARGET, skipping" >&2
+      continue
+    fi
+  fi
+  # now local target branch is up-to-date
+
+  echo "merging with $BASE..."
+  git merge $BASE
+  if [ $? -ne 0 ]
+  then
+    echo "merge failed, skipping" >&2
+    continue
+  fi
+
+  echo "pushing to $REMOTE..."
+  git push -u $REMOTE $TARGET
+done

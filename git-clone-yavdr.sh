debf555ebd4ee26ec12d803a549cda815508e745
Steffen Barszus <steffenbpunkt@googlemail.com>
2013-04-28 10:01:24 +0200

    fix fetch of repository

--- git-clone-yavdr.sh
+++ git-clone-yavdr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # clone or pull repos
-for REPO in `curl -s https://api.github.com/users/yavdr/repos?per_page=200 | grep name | sort | awk -F \" '{ print $4 }'`
+for REPO in `curl -s https://api.github.com/users/yavdr/repos?per_page=200 | grep '    "name"' | sort | awk -F \" '{ print $4 }'`
 do
   if [ ! -d $REPO ]; then
     if [ "$READONLY" = "1" ]; then

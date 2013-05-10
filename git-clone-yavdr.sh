79c4d4df2c52a12b532b7e84000114aab5166884
Lars Hanisch <dvb@flensrocker.de>
2013-05-10 10:55:49 +0200

    use full_name for repository name, name is ambigous

--- git-clone-yavdr.sh
+++ git-clone-yavdr.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # clone or pull repos
-for REPO in `curl -s https://api.github.com/users/yavdr/repos?per_page=200 | grep '    "name"' | sort | awk -F \" '{ print $4 }'`
+for REPO in `curl -s https://api.github.com/users/yavdr/repos?per_page=200 | grep full_name | sort | awk -F \" '{ print $4 }'`
 do
+  DIR=`echo $REPO | awk -F / '{ print $2 }'`
+  echo $DIR
   if [ ! -d $REPO ]; then
     if [ "$READONLY" = "1" ]; then
-      git clone git://github.com/yavdr/${REPO}.git
+      git clone git://github.com/${REPO}.git
     else
-      git clone git@github.com:yavdr/${REPO}.git
+      git clone git@github.com:${REPO}.git
     fi
-    cd $REPO
-    git pull --all&
-    cd ..
   else
-    cd $REPO
+    cd $DIR
     git pull --all&
     cd ..
   fi
